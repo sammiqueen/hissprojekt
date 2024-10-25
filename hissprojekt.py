7f91536c4524cf90f949df81dae6b575c9c3bb99
sammiqueen <146732761+sammiqueen@users.noreply.github.com>
2024-10-25 09:36:10 +0200

    Fixade massor av logik och andra saker. Byggde upp olika klasser med olika logiska funktioner. Lade till lite extra i hissprojekt_PSEUDO om mitt tänkande idag.

--- hissprojekt.py
+++ hissprojekt.py
@@ -1,9 +1,35 @@
-# HISS CORE
+def moveElevator(queue) :
 
-def core() :
-    queue = [1,2]
+    currentFloor = queue[0]
+    queue.remove(currentFloor)
 
-    for i in queue:
-        print (i)
+    print (currentFloor)
+    return currentFloor
 
-core()
+def queueSort(queue, currentFloor) :
+
+    if (queue[0] > currentFloor):
+        goingDown = False
+    else:
+        goingDown = True
+
+    queue.sort(reverse = goingDown)
+
+    print (queue)
+    return queue, goingDown
+
+def buttonPress(queue, waitQueue, goingDown):
+
+    while True:
+        try:
+            buttonPress = int(input("Önskad våning?"))
+            break
+        except:
+            print("Endast integers tack")
+
+    if (goingDown and (buttonPress <= queue [0]) or (not goingDown) and (buttonPress >= queue[0])):
+        queue.append(buttonPress)
+    else:
+        waitQueue.append(buttonPress)
+
+queueSort()
